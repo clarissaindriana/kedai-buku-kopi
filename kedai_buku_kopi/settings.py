65aeb4792c8d0c3737c6a2b6b32d3117a2f8d083
clarissaindriana <clarissaindrianapramesti@gmail.com>
2024-09-19 13:31:47 +0700

    adding autentikasi, session, dan cookies

--- kedai_buku_kopi/settings.py
+++ kedai_buku_kopi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-ihf3y4)4(ky43!x8a=nzs9=-7$2julpg9(fnmg#^jgo!fvea-0"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "clarissa-indriana-kedaibukukopi.pbp.cs.ui.ac.id"]
 
